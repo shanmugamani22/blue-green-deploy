b718801f593db23f31de801bb2a1605b47c17bb2
Shanmugamani <141019852+shanmugamani22@users.noreply.github.com>
2025-07-22 11:32:30 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-set -e
-
 echo "[after_install] Running after_install.sh script"
-echo "[after_install] Current directory: $(pwd)"
-echo "[after_install] Listing files in current directory:"
-ls -al
 
-if [ -d "html" ]; then
-  echo "[after_install] html directory found. Copying files..."
-  cp -r html/* /var/www/html/
+APP_DIR=$(dirname "$(readlink -f "$0")")/..
+
+echo "[after_install] App directory resolved to $APP_DIR"
+
+if [ -d "$APP_DIR/html" ]; then
+    echo "Copying new HTML to Apache directory..."
+    cp -r "$APP_DIR/html/"* /var/www/html/
 else
-  echo "[after_install] html directory NOT found!"
-  exit 1
+    echo "[after_install] html directory NOT found!"
+    exit 1
 fi
-
